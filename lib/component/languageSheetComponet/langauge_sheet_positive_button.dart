f7e7654b1ae3858cb3431c001e2b769a7141d1d0
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-19 01:51:55 +0530

    :bug: BugFix: Cancel button reseting the Language

--- lib/component/languageSheetComponet/langauge_sheet_positive_button.dart
+++ lib/component/languageSheetComponet/langauge_sheet_positive_button.dart
@@ -1,6 +1,8 @@
+import 'package:arre_assignment_flutter/bloc/language_bloc.dart';
 import 'package:arre_assignment_flutter/config/text_style.dart' as tstyle;
 import 'package:arre_assignment_flutter/config/theme/abstract_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LanguageSheetPositiveButton extends StatelessWidget {
   const LanguageSheetPositiveButton({
@@ -12,20 +14,25 @@ class LanguageSheetPositiveButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: th.bottomSheetPositiveBtnBackground,
-      ),
-      onPressed: () {
-        Navigator.pop(context);
+    return Consumer(
+      builder: (BuildContext context, WidgetRef ref, Widget? child) {
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: th.bottomSheetPositiveBtnBackground,
+          ),
+          onPressed: () {
+            ref.read(languageprovider.notifier).finalUpdate();
+            Navigator.pop(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Update",
+              style: tstyle.btmSheetPositiveButtonTextStyle,
+            ),
+          ),
+        );
       },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Text(
-          "Update",
-          style: tstyle.btmSheetPositiveButtonTextStyle,
-        ),
-      ),
     );
   }
 }
